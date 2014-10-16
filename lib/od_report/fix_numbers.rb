70110b06f454b528d6998e708e698e42c93a9cdd
Nikolay Norkin <n.e.norkin@gmail.com>
2014-10-16 23:11:50 +0400

    Fixing both Floats and BigDecimals

--- lib/od_report/fix_numbers.rb
+++ lib/od_report/fix_numbers.rb
@@ -1,12 +1,20 @@
-module FixFloat
+module FixNumbers
   DELIMITED_REGEX = /(\d)(?=(\d\d\d)+(?!\d))/
   DELIMETER = ' '
 
-  refine Float do
+  module Fixer
     def to_s
-      left, right = round(2).inspect.to_s.split('.')
+      left, right = round(2).to_s.split('.')
       left.gsub!(DELIMITED_REGEX) { "#{$1}#{DELIMETER}" }
       [left, right].compact.join(',')
     end
+  end
+
+  refine Float do
+    include Fixer
+  end
+
+  refine BigDecimal do
+    include Fixer
   end
 end
